15fda26b5c076a554771b58cb8307c2645f6f7d3
Melvin Renner <melvin.renner@googlemail.com>
2024-03-21 03:18:27 +0100

    initial commit

--- config/general.config.js
+++ config/general.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    clientURL: 'http://65.21.111.146',
+    clientURL: 'http://65.21.111.164',
     clientURLhttps: 'http://localhost:3000',
     localClientURL:'http://localhost:3000',
     clientKundeURL: 'http://localhost:3000',
